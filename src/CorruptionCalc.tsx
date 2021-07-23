07811d43f641617389f770bd1ffa51962e22c750
Teavana-T <iamteavana@gmail.com>
2021-07-23 18:44:24 +0100

    Added value filtering on Corruption state

--- src/CorruptionCalc.tsx
+++ src/CorruptionCalc.tsx
@@ -2,21 +2,42 @@ import React, { Component } from 'react';
 import { Button, Container, Divider, Header, Input, Label, Placeholder, Segment } from 'semantic-ui-react';
 import update from 'immutability-helper';
 
-class CorruptionCalc extends Component<{}, { mod: number, c: number, mon: number }>  {
+class StyledInput extends Component {
+
+}
+
+class CorruptionCalc extends Component<{}, { mod: number, c: number, mon: number, calcMon: number }>  {
   constructor(props: any) {
     super(props)
 
     this.state = {
       mod: 0,
       c: 0,
-      mon: 0
+      mon: 0,
+      calcMon: 0
     }
 
     this.onChange = this.onChange.bind(this);
+    this.calculateCoT = this.calculateCoT.bind(this);
   }
 
   onChange(e: any, d: any) {
-    let newData: any = update(this.state, { [d.name]: { $set: d.value } });
+    let newData: any;
+    if (d.name === 'c') {
+      if (d.value > 100) {
+        newData = update(this.state, { [d.name]: { $set: 100 } });
+        console.log('>100');
+      } else if (d.value < 0) {
+        newData = update(this.state, { [d.name]: { $set: 0 } });
+        console.log('<0');
+      } else {
+        newData = update(this.state, { [d.name]: { $set: d.value } });
+      }
+    } else {
+      newData = update(this.state, { [d.name]: { $set: d.value } });
+    }
+
+
 
     this.setState(newData);
   }
@@ -28,17 +49,15 @@ class CorruptionCalc extends Component<{}, { mod: number, c: number, mon: number
     let months: number = mon;
 
     for (
-      let i: number = corruption*1;
+      let i: number = corruption * 1;
       i > 0;
-      i = i + ((i*1 / -20) / 12) + (modifier*1 / 12)
+      i = i + ((i * 1 / -20) / 12) + (modifier * 1 / 12)
     ) {
       months++;
     }
-    console.log('modifier: ' + modifier);
-    console.log('corruption: ' + corruption);
-    console.log('months: ' + months);
-    console.log(c + ' ' + mon);
-    return (months)
+    const newData = update(this.state, { calcMon: { $set: months } });
+
+    this.setState(newData);
   }
 
   render() {
@@ -50,8 +69,8 @@ class CorruptionCalc extends Component<{}, { mod: number, c: number, mon: number
           <Input type='number' name='mod' onChange={this.onChange} value={this.state.mod} />
           <Input type='number' name='c' onChange={this.onChange} value={this.state.c} />
           <Input type='number' name='mon' onChange={this.onChange} value={this.state.mon} />
-          <Label content={'It will take ' + this.calculateCoT(this.state.mod, this.state.c, this.state.mon) + ' months to burn to the ground'} />
-          <Button onClick={() => console.log(this.state)} />
+          <Label content={'It will take ' + this.state.calcMon + ' months to burn to the ground'} />
+          <Button onClick={() => this.calculateCoT(this.state.mod, this.state.c, this.state.mon)} />
         </Segment>
       </Container>
     );
