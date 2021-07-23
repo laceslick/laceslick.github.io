c9ea351d501f3ec64fe76937c0e885ba450dbd9e
Teavana-T <iamteavana@gmail.com>
2021-07-23 17:15:11 +0100

    NavBar added, Placeholders added

---
+++ src/NavBar.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Menu, Dropdown, Image, Input } from 'semantic-ui-react';
+
+class NavBar extends Component {
+    render() {
+        return (
+            <Menu attached='bottom' inverted>
+                <Menu.Item style={{ padding: 5 }} >
+                    <Image src='https://cdn.discordapp.com/attachments/236152279839801344/868161637671137300/unknown.png' size='mini' spaced='right' />
+                </Menu.Item>
+                <Menu.Item header>
+                    LaceSlick exists.
+                </Menu.Item>
+                <Menu.Item fitted >
+                    <Input list='projects' placeholder='Search...' icon='search' />
+                    <datalist id='projects'>
+                        <option value='Corruption Calculator'>Corruption Calculator</option>
+                        <option value='Home'>Home</option>
+                    </datalist>
+                </Menu.Item>
+                <Menu.Menu position='right'>
+                    <Dropdown item text="Tools">
+                        <Dropdown.Menu>
+                            <Dropdown.Item content="haha pp" >
+                                <Image src='https://cdn.discordapp.com/attachments/236152279839801344/868161871721680936/unknown.png' />
+                                pp
+                            </Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    <Menu.Item
+                        content="Home"
+                    />
+                </Menu.Menu>
+            </Menu>
+        )
+    }
+}
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'semantic-ui-css/semantic.min.css';
 import reportWebVitals from './reportWebVitals';
+import CorruptionCalc from './App';
+import NavBar from './NavBar';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <NavBar />
+    <CorruptionCalc />
   </React.StrictMode>,
   document.getElementById('root')
 );
